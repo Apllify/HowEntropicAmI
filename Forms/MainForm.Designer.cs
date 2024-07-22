f8ffd6f2b2a2b7c6f7cf6199f94f4bed86cd140c
Apllify <ralilahlou50@gmail.com>
2024-07-22 13:53:16 +0100

    Change UI for personal entropy

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace HowEntropicAmI.Forms
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			titleLabel = new Label();
+			passwordInput = new TextBox();
+			passwordConfirmButton = new Button();
+			bfTitleLabel = new Label();
+			bfEntropyLabel = new Label();
+			errorLabel = new Label();
+			cbfTitleLabel = new Label();
+			cbfEntropyLabel = new Label();
+			toolTip = new ToolTip(components);
+			daTitleLabel = new Label();
+			daEntropyLabel = new Label();
+			idaTitleLabel = new Label();
+			idaEntropyLabel = new Label();
+			idaCustomizeButton = new Button();
+			worstCaseTitleLabel = new Label();
+			worstCaseCountLabel = new Label();
+			SuspendLayout();
+			// 
+			// titleLabel
+			// 
+			titleLabel.Anchor = AnchorStyles.Top;
+			titleLabel.BackColor = SystemColors.Control;
+			titleLabel.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+			titleLabel.Location = new Point(338, 32);
+			titleLabel.Name = "titleLabel";
+			titleLabel.Size = new Size(355, 46);
+			titleLabel.TabIndex = 0;
+			titleLabel.Text = "Enter your password :";
+			titleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// passwordInput
+			// 
+			passwordInput.Anchor = AnchorStyles.Top;
+			passwordInput.Location = new Point(338, 100);
+			passwordInput.Name = "passwordInput";
+			passwordInput.Size = new Size(285, 27);
+			passwordInput.TabIndex = 1;
+			// 
+			// passwordConfirmButton
+			// 
+			passwordConfirmButton.Anchor = AnchorStyles.Top;
+			passwordConfirmButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+			passwordConfirmButton.Location = new Point(647, 100);
+			passwordConfirmButton.Name = "passwordConfirmButton";
+			passwordConfirmButton.Size = new Size(46, 26);
+			passwordConfirmButton.TabIndex = 2;
+			passwordConfirmButton.Text = "Run";
+			passwordConfirmButton.UseVisualStyleBackColor = true;
+			passwordConfirmButton.Click += passwordConfirmButton_Click;
+			// 
+			// bfTitleLabel
+			// 
+			bfTitleLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			bfTitleLabel.AutoSize = true;
+			bfTitleLabel.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			bfTitleLabel.Location = new Point(27, 291);
+			bfTitleLabel.Name = "bfTitleLabel";
+			bfTitleLabel.Size = new Size(160, 35);
+			bfTitleLabel.TabIndex = 3;
+			bfTitleLabel.Text = "Brute Force : ";
+			bfTitleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// bfEntropyLabel
+			// 
+			bfEntropyLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			bfEntropyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			bfEntropyLabel.ForeColor = SystemColors.GrayText;
+			bfEntropyLabel.Location = new Point(27, 344);
+			bfEntropyLabel.Name = "bfEntropyLabel";
+			bfEntropyLabel.Size = new Size(160, 71);
+			bfEntropyLabel.TabIndex = 4;
+			bfEntropyLabel.Text = "---";
+			bfEntropyLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// errorLabel
+			// 
+			errorLabel.Anchor = AnchorStyles.Top;
+			errorLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			errorLabel.ForeColor = Color.Goldenrod;
+			errorLabel.Location = new Point(338, 140);
+			errorLabel.Name = "errorLabel";
+			errorLabel.Size = new Size(355, 94);
+			errorLabel.TabIndex = 5;
+			errorLabel.TextAlign = ContentAlignment.TopCenter;
+			// 
+			// cbfTitleLabel
+			// 
+			cbfTitleLabel.Anchor = AnchorStyles.Bottom;
+			cbfTitleLabel.AutoSize = true;
+			cbfTitleLabel.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			cbfTitleLabel.Location = new Point(227, 291);
+			cbfTitleLabel.Name = "cbfTitleLabel";
+			cbfTitleLabel.Size = new Size(228, 35);
+			cbfTitleLabel.TabIndex = 6;
+			cbfTitleLabel.Text = "Clever Brute Force :";
+			cbfTitleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// cbfEntropyLabel
+			// 
+			cbfEntropyLabel.Anchor = AnchorStyles.Bottom;
+			cbfEntropyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			cbfEntropyLabel.ForeColor = SystemColors.GrayText;
+			cbfEntropyLabel.Location = new Point(227, 344);
+			cbfEntropyLabel.Name = "cbfEntropyLabel";
+			cbfEntropyLabel.Size = new Size(228, 71);
+			cbfEntropyLabel.TabIndex = 7;
+			cbfEntropyLabel.Text = "---";
+			cbfEntropyLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// daTitleLabel
+			// 
+			daTitleLabel.Anchor = AnchorStyles.Bottom;
+			daTitleLabel.AutoSize = true;
+			daTitleLabel.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			daTitleLabel.Location = new Point(487, 291);
+			daTitleLabel.Name = "daTitleLabel";
+			daTitleLabel.Size = new Size(216, 35);
+			daTitleLabel.TabIndex = 8;
+			daTitleLabel.Text = "Dictionary Attack :";
+			daTitleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// daEntropyLabel
+			// 
+			daEntropyLabel.Anchor = AnchorStyles.Bottom;
+			daEntropyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			daEntropyLabel.ForeColor = SystemColors.GrayText;
+			daEntropyLabel.Location = new Point(487, 344);
+			daEntropyLabel.Name = "daEntropyLabel";
+			daEntropyLabel.Size = new Size(206, 71);
+			daEntropyLabel.TabIndex = 9;
+			daEntropyLabel.Text = "---";
+			daEntropyLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// idaTitleLabel
+			// 
+			idaTitleLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			idaTitleLabel.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			idaTitleLabel.Location = new Point(749, 254);
+			idaTitleLabel.Name = "idaTitleLabel";
+			idaTitleLabel.Size = new Size(227, 108);
+			idaTitleLabel.TabIndex = 10;
+			idaTitleLabel.Text = "Personalized Dictionary Attack :";
+			idaTitleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// idaEntropyLabel
+			// 
+			idaEntropyLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			idaEntropyLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			idaEntropyLabel.ForeColor = SystemColors.GrayText;
+			idaEntropyLabel.Location = new Point(749, 344);
+			idaEntropyLabel.Name = "idaEntropyLabel";
+			idaEntropyLabel.Size = new Size(227, 71);
+			idaEntropyLabel.TabIndex = 11;
+			idaEntropyLabel.Text = "---";
+			idaEntropyLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// idaCustomizeButton
+			// 
+			idaCustomizeButton.Location = new Point(791, 235);
+			idaCustomizeButton.Name = "idaCustomizeButton";
+			idaCustomizeButton.Size = new Size(136, 29);
+			idaCustomizeButton.TabIndex = 12;
+			idaCustomizeButton.Text = "Customize";
+			idaCustomizeButton.UseVisualStyleBackColor = true;
+			idaCustomizeButton.Click += idaCustomizeButton_Click;
+			// 
+			// worstCaseTitleLabel
+			// 
+			worstCaseTitleLabel.Anchor = AnchorStyles.Bottom;
+			worstCaseTitleLabel.AutoSize = true;
+			worstCaseTitleLabel.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			worstCaseTitleLabel.Location = new Point(207, 466);
+			worstCaseTitleLabel.Name = "worstCaseTitleLabel";
+			worstCaseTitleLabel.Size = new Size(301, 35);
+			worstCaseTitleLabel.TabIndex = 13;
+			worstCaseTitleLabel.Text = "Your worst case entropy : ";
+			worstCaseTitleLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// worstCaseCountLabel
+			// 
+			worstCaseCountLabel.Anchor = AnchorStyles.Bottom;
+			worstCaseCountLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			worstCaseCountLabel.ForeColor = Color.Red;
+			worstCaseCountLabel.Location = new Point(532, 450);
+			worstCaseCountLabel.Name = "worstCaseCountLabel";
+			worstCaseCountLabel.Size = new Size(206, 71);
+			worstCaseCountLabel.TabIndex = 14;
+			worstCaseCountLabel.Text = "---";
+			worstCaseCountLabel.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1017, 570);
+			Controls.Add(worstCaseCountLabel);
+			Controls.Add(worstCaseTitleLabel);
+			Controls.Add(idaCustomizeButton);
+			Controls.Add(idaEntropyLabel);
+			Controls.Add(idaTitleLabel);
+			Controls.Add(daEntropyLabel);
+			Controls.Add(daTitleLabel);
+			Controls.Add(cbfEntropyLabel);
+			Controls.Add(cbfTitleLabel);
+			Controls.Add(errorLabel);
+			Controls.Add(bfEntropyLabel);
+			Controls.Add(bfTitleLabel);
+			Controls.Add(passwordConfirmButton);
+			Controls.Add(passwordInput);
+			Controls.Add(titleLabel);
+			Name = "MainForm";
+			Text = "How Entropic Am I ?";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label titleLabel;
+		private TextBox passwordInput;
+		private Button passwordConfirmButton;
+		private Label bfTitleLabel;
+		private Label bfEntropyLabel;
+		private Label errorLabel;
+		private Label cbfTitleLabel;
+		private Label cbfEntropyLabel;
+		private ToolTip toolTip;
+		private Label daTitleLabel;
+		private Label daEntropyLabel;
+		private Label idaTitleLabel;
+		private Label idaEntropyLabel;
+		private Button idaCustomizeButton;
+		private Label worstCaseTitleLabel;
+		private Label worstCaseCountLabel;
+	}
+}
---
+++ Forms/QuestionForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace HowEntropicAmI.Forms
+{
+	partial class QuestionForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			disclaimerLabel = new Label();
+			questionLabel1 = new Label();
+			questionInput1 = new TextBox();
+			questionInput2 = new TextBox();
+			questionLabel2 = new Label();
+			questionInput3 = new TextBox();
+			questionLabel3 = new Label();
+			questionInput4 = new TextBox();
+			questionLabel4 = new Label();
+			questionInput5 = new TextBox();
+			questionLabel5 = new Label();
+			questionInput6 = new TextBox();
+			questionLabel6 = new Label();
+			questionInput7 = new TextBox();
+			questionLabel7 = new Label();
+			questionInput8 = new TextBox();
+			questionLabel8 = new Label();
+			questionInput9 = new TextBox();
+			questionLabel9 = new Label();
+			SuspendLayout();
+			// 
+			// disclaimerLabel
+			// 
+			disclaimerLabel.Anchor = AnchorStyles.Top;
+			disclaimerLabel.Font = new Font("Segoe UI", 10.8F, FontStyle.Italic, GraphicsUnit.Point);
+			disclaimerLabel.ForeColor = SystemColors.ControlDarkDark;
+			disclaimerLabel.ImageAlign = ContentAlignment.BottomLeft;
+			disclaimerLabel.Location = new Point(65, 20);
+			disclaimerLabel.Name = "disclaimerLabel";
+			disclaimerLabel.Size = new Size(688, 61);
+			disclaimerLabel.TabIndex = 0;
+			disclaimerLabel.Text = "If an answer requires multiple words, please separate them with spaces (and nothing else)\r\n";
+			disclaimerLabel.TextAlign = ContentAlignment.TopCenter;
+			// 
+			// questionLabel1
+			// 
+			questionLabel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel1.Location = new Point(26, 99);
+			questionLabel1.Name = "questionLabel1";
+			questionLabel1.RightToLeft = RightToLeft.No;
+			questionLabel1.Size = new Size(229, 46);
+			questionLabel1.TabIndex = 1;
+			questionLabel1.Text = "What is your full name ?";
+			questionLabel1.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel1.UseMnemonic = false;
+			// 
+			// questionInput1
+			// 
+			questionInput1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput1.Location = new Point(65, 145);
+			questionInput1.Name = "questionInput1";
+			questionInput1.Size = new Size(152, 27);
+			questionInput1.TabIndex = 2;
+			// 
+			// questionInput2
+			// 
+			questionInput2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput2.Location = new Point(342, 145);
+			questionInput2.Name = "questionInput2";
+			questionInput2.Size = new Size(152, 27);
+			questionInput2.TabIndex = 4;
+			// 
+			// questionLabel2
+			// 
+			questionLabel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel2.Location = new Point(303, 99);
+			questionLabel2.Name = "questionLabel2";
+			questionLabel2.RightToLeft = RightToLeft.No;
+			questionLabel2.Size = new Size(229, 46);
+			questionLabel2.TabIndex = 3;
+			questionLabel2.Text = "What are your pets' names ? ";
+			questionLabel2.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel2.UseMnemonic = false;
+			// 
+			// questionInput3
+			// 
+			questionInput3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput3.Location = new Point(601, 145);
+			questionInput3.Name = "questionInput3";
+			questionInput3.Size = new Size(152, 27);
+			questionInput3.TabIndex = 6;
+			// 
+			// questionLabel3
+			// 
+			questionLabel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel3.Location = new Point(562, 99);
+			questionLabel3.Name = "questionLabel3";
+			questionLabel3.RightToLeft = RightToLeft.No;
+			questionLabel3.Size = new Size(229, 46);
+			questionLabel3.TabIndex = 5;
+			questionLabel3.Text = "What is your birthday ? (dd/mm/yyyy)";
+			questionLabel3.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel3.UseMnemonic = false;
+			// 
+			// questionInput4
+			// 
+			questionInput4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput4.Location = new Point(65, 266);
+			questionInput4.Name = "questionInput4";
+			questionInput4.Size = new Size(152, 27);
+			questionInput4.TabIndex = 8;
+			// 
+			// questionLabel4
+			// 
+			questionLabel4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel4.Location = new Point(26, 220);
+			questionLabel4.Name = "questionLabel4";
+			questionLabel4.RightToLeft = RightToLeft.No;
+			questionLabel4.Size = new Size(229, 46);
+			questionLabel4.TabIndex = 7;
+			questionLabel4.Text = "What are your children's names ?";
+			questionLabel4.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel4.UseMnemonic = false;
+			// 
+			// questionInput5
+			// 
+			questionInput5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput5.Location = new Point(342, 266);
+			questionInput5.Name = "questionInput5";
+			questionInput5.Size = new Size(152, 27);
+			questionInput5.TabIndex = 10;
+			// 
+			// questionLabel5
+			// 
+			questionLabel5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel5.Location = new Point(303, 220);
+			questionLabel5.Name = "questionLabel5";
+			questionLabel5.RightToLeft = RightToLeft.No;
+			questionLabel5.Size = new Size(229, 46);
+			questionLabel5.TabIndex = 9;
+			questionLabel5.Text = "What is your e-mail address ?";
+			questionLabel5.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel5.UseMnemonic = false;
+			// 
+			// questionInput6
+			// 
+			questionInput6.Location = new Point(601, 266);
+			questionInput6.Name = "questionInput6";
+			questionInput6.Size = new Size(152, 27);
+			questionInput6.TabIndex = 12;
+			// 
+			// questionLabel6
+			// 
+			questionLabel6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel6.Location = new Point(562, 220);
+			questionLabel6.Name = "questionLabel6";
+			questionLabel6.RightToLeft = RightToLeft.No;
+			questionLabel6.Size = new Size(229, 46);
+			questionLabel6.TabIndex = 11;
+			questionLabel6.Text = "What are your nicknames ?";
+			questionLabel6.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel6.UseMnemonic = false;
+			// 
+			// questionInput7
+			// 
+			questionInput7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput7.Location = new Point(65, 381);
+			questionInput7.Name = "questionInput7";
+			questionInput7.Size = new Size(152, 27);
+			questionInput7.TabIndex = 14;
+			// 
+			// questionLabel7
+			// 
+			questionLabel7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel7.Location = new Point(26, 335);
+			questionLabel7.Name = "questionLabel7";
+			questionLabel7.RightToLeft = RightToLeft.No;
+			questionLabel7.Size = new Size(229, 46);
+			questionLabel7.TabIndex = 13;
+			questionLabel7.Text = "What is your full name (separated by spaces) ?";
+			questionLabel7.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel7.UseMnemonic = false;
+			// 
+			// questionInput8
+			// 
+			questionInput8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput8.Location = new Point(342, 381);
+			questionInput8.Name = "questionInput8";
+			questionInput8.Size = new Size(152, 27);
+			questionInput8.TabIndex = 16;
+			// 
+			// questionLabel8
+			// 
+			questionLabel8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel8.Location = new Point(303, 335);
+			questionLabel8.Name = "questionLabel8";
+			questionLabel8.RightToLeft = RightToLeft.No;
+			questionLabel8.Size = new Size(229, 46);
+			questionLabel8.TabIndex = 15;
+			questionLabel8.Text = "What is your full name (separated by spaces) ?";
+			questionLabel8.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel8.UseMnemonic = false;
+			// 
+			// questionInput9
+			// 
+			questionInput9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionInput9.Location = new Point(601, 381);
+			questionInput9.Name = "questionInput9";
+			questionInput9.Size = new Size(152, 27);
+			questionInput9.TabIndex = 18;
+			// 
+			// questionLabel9
+			// 
+			questionLabel9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			questionLabel9.ForeColor = SystemColors.ActiveCaptionText;
+			questionLabel9.Location = new Point(562, 335);
+			questionLabel9.Name = "questionLabel9";
+			questionLabel9.RightToLeft = RightToLeft.No;
+			questionLabel9.Size = new Size(229, 46);
+			questionLabel9.TabIndex = 17;
+			questionLabel9.Text = "Feel free to add any extra relevant words here :";
+			questionLabel9.TextAlign = ContentAlignment.MiddleCenter;
+			questionLabel9.UseMnemonic = false;
+			// 
+			// QuestionForm
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(827, 453);
+			Controls.Add(questionInput9);
+			Controls.Add(questionLabel9);
+			Controls.Add(questionInput8);
+			Controls.Add(questionLabel8);
+			Controls.Add(questionInput7);
+			Controls.Add(questionLabel7);
+			Controls.Add(questionInput6);
+			Controls.Add(questionLabel6);
+			Controls.Add(questionInput5);
+			Controls.Add(questionLabel5);
+			Controls.Add(questionInput4);
+			Controls.Add(questionLabel4);
+			Controls.Add(questionInput3);
+			Controls.Add(questionLabel3);
+			Controls.Add(questionInput2);
+			Controls.Add(questionLabel2);
+			Controls.Add(questionInput1);
+			Controls.Add(questionLabel1);
+			Controls.Add(disclaimerLabel);
+			Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+			Name = "QuestionForm";
+			Text = "How Entropic Am I ? - Customization";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label disclaimerLabel;
+		private Label questionLabel1;
+		private TextBox questionInput1;
+		private TextBox questionInput2;
+		private Label questionLabel2;
+		private TextBox questionInput3;
+		private Label questionLabel3;
+		private TextBox questionInput4;
+		private Label questionLabel4;
+		private TextBox questionInput5;
+		private Label questionLabel5;
+		private TextBox questionInput6;
+		private Label questionLabel6;
+		private TextBox questionInput7;
+		private Label questionLabel7;
+		private TextBox questionInput8;
+		private Label questionLabel8;
+		private TextBox questionInput9;
+		private Label questionLabel9;
+	}
+}
